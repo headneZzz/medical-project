3312218ed0646507b1b1185127d06dfc5f03a51e
Ilya Krutko <mr.darkilya@mail.ru>
2022-05-20 22:43:10 +0300

    Initial commit

---
+++ src/main/java/com/headnezzz/medicalproject/MedicalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.headnezzz.medicalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/headnezzz/medicalproject/MedicalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.headnezzz.medicalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
